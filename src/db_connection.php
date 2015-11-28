c9f52f2da2e1cc37aca87ec508235abc5a8a996b
kiran4399 <kiran4399@gmail.com>
2015-11-28 22:52:00 +0530

    php modules added

---
+++ src/db_connection.php
@@ -0,0 +1,11 @@
+
+    <?php  
+    /** 
+     * Created by PhpStorm. 
+     * User: Ehtesham Mehmood 
+     * Date: 11/21/2014 
+     * Time: 1:13 AM 
+     */  
+    $dbcon=mysqli_connect("localhost","root","");  
+    mysqli_select_db($dbcon,"event_manager");  
+    ?>  
