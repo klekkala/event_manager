7597f3709d40895d190682708335b489eeeeea4c
kiran4399 <kiran4399@gmail.com>
2015-11-30 18:00:05 +0530

    pre-final

--- src/db_connection.php
+++ src/db_connection.php
@@ -6,6 +6,6 @@
      * Date: 11/21/2014 
      * Time: 1:13 AM 
      */  
-    $dbcon=mysqli_connect("localhost","root","");  
+    $dbcon=mysqli_connect("localhost","root","kiran");  
     mysqli_select_db($dbcon,"event_manager");  
     ?>  
