efff4c617f4cc9737429e5352ed992bc7376b1e4
kiran4399 <kiran4399@gmail.com>
2015-12-01 11:43:31 +0530

    database driver fixed

--- src/registration.php
+++ src/registration.php
@@ -1,66 +1,14 @@
-
-    <html>  
-    <head lang="en">  
-        <meta charset="UTF-8">  
-        <link type="text/css" rel="stylesheet" href="bootstrap-3.2.0-dist\css\bootstrap.css">  
-        <title>Registration</title>  
-    </head>  
-    <style>  
-        .login-panel {  
-            margin-top: 150px;  
-      
-    </style>  
-    <body>  
-      
-    <div class="container"><!-- container class is used to centered  the body of the browser with some decent width-->  
-        <div class="row"><!-- row class is used for grid system in Bootstrap-->  
-            <div class="col-md-4 col-md-offset-4"><!--col-md-4 is used to create the no of colums in the grid also use for medimum and large devices-->  
-                <div class="login-panel panel panel-success">  
-                    <div class="panel-heading">  
-                        <h3 class="panel-title">Registration</h3>  
-                    </div>  
-                    <div class="panel-body">  
-                        <form role="form" method="post" action="registration.php">  
-                            <fieldset>  
-                                <div class="form-group">  
-                                    <input class="form-control" placeholder="Username" name="name" type="text" autofocus>  
-                                </div>  
-      
-                                <div class="form-group">  
-                                    <input class="form-control" placeholder="E-mail" name="email" type="email" autofocus>  
-                                </div>  
-                                <div class="form-group">  
-                                    <input class="form-control" placeholder="Password" name="pass" type="password" value="">  
-                                </div>  
-      
-      
-                                <input class="btn btn-lg btn-success btn-block" type="submit" value="register" name="register" >  
-      
-                            </fieldset>  
-                        </form>  
-                        <center><b>Already registered ?</b> <br></b><a href="login.php">Login here</a></center><!--for centered text-->  
-                    </div>  
-                </div>  
-            </div>  
-        </div>  
-    </div>  
-      
-    </body>  
-      
-    </html>  
-      
+   
     <?php  
-      
     include("db_connection.php");//make connection here  
+
     if(isset($_POST['register']))  
     {  
-        //$user_name=$_POST['name'];//here getting result from the post array after submitting the form.  
-        //$user_pass=$_POST['pass'];//same  
-        //$user_email=$_POST['email'];//same  
-      
-$user_name = 'kiran4399';
-$user_email = 'kiran4399@gmail.com';
-$user_pass = 'helloworld';
+        $user_name=$_POST['name'];//here getting result from the post array after submitting the form.  
+        $user_pass=$_POST['pass'];//same  
+        $user_email=$_POST['email'];//same  
+  
+ echo 4399;
       
         if($user_name=='')  
         {  
@@ -89,13 +37,17 @@ $user_pass = 'helloworld';
     echo "<script>alert('Email $user_email is already exist in our database, Please try another one!')</script>";  
     exit();  
         }  
-    //insert the user into the database.  
-        $insert_user="insert into membersinformationTable (mID,first_name,last_name,email,uname,pass,regdate,id) VALUE (1,'kiran','kumar','$user_email','$user_name','$user_pass',20120618, 3)";  
+    //insert the user into the database. 
+        echo 4399; 
+        $insert_user="insert into membersinformationTable (first_name,last_name,email,uname,pass,regdate,id) VALUE ('kiran','kumar','$user_email','$user_name','$user_pass',20120618, 3)";  
         if(mysqli_query($dbcon,$insert_user))  
         {  
             echo"<script>window.open('welcome.php','_self')</script>";  
         }  
-      
+        else
+{
+echo "not done";
+}
       
       
     }  
